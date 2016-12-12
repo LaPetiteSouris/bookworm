72aff8b2329b96764cbfeddc432c7e71c9115ec6
LaPetiteSouris <sontunghoang@gmail.com>
2016-12-12 17:58:12 +0100

    Temporary disable workers

--- api/server.py
+++ api/server.py
@@ -1,7 +1,6 @@
 import atexit
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
+
 from flask import Flask
 
 from api import config
@@ -16,25 +15,7 @@ app = Flask(__name__)
 app.register_blueprint(book_api)
 app.register_blueprint(error_handler_api)
 
-# Setup cron
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(
-    func=book_hunter.launch,
-    trigger=IntervalTrigger(hours=4),
-    id='book_hunter',
-    name='Hunt for book from Google  Book API',
-    replace_existing=False)
 
-scheduler.add_job(
-    func=distance_calculator.launch,
-    trigger=IntervalTrigger(hours=4),
-    id='distance_calculator',
-    name='Calculate distance between users',
-    replace_existing=False)
-
-# Shut down the scheduler when exiting the app
-atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == "__main__":
     log.info('service ready', extra={'server': {'host': config.HOST, 'port': config.PORT}})
